3e9e6aa7121925c15f3fe76f4060398ec1ea4d9c
Danylo <dashko950@gmail.com>
2026-03-29 21:22:00 +0200

    movesapp

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app/App";
+import App from "./components/app/App";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
 
